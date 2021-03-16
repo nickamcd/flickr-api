56cc6fabaa5f527dd0b209d3d4af0e5217835ead
Nick McDaniel <nickamcd@gmail.com>
2021-03-16 12:30:48 -0700

    Returns photos from the user id entered

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,9 +2,17 @@ class StaticPagesController < ApplicationController
   require 'flickr'
 
   def index
-   flickr = Flickr.new ENV['flickr_api_key'], ENV['flickr_api_secret']
+    flickr = Flickr.new ENV['flickr_api_key'], ENV['flickr_api_secret']
 
-   
+    if params[:user]
+      begin
+        @photos = flickr.people.getPhotos(user_id: params[:user])
+      rescue Flickr::FailedResponse
+        flash.now[:alert] = 'Unable to find that user\'s photos'
+      end
+    else
+      render :index
+    end
 
   end
 
