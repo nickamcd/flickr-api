c42ba0fab8a1679398cae5d9fb56c6f2fe55db6c
Nick McDaniel <nickamcd@gmail.com>
2021-03-16 11:38:53 -0700

    Created basic show page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,2 +1,11 @@
 class StaticPagesController < ApplicationController
+  require 'flickr'
+
+  def index
+    # flickr = Flickr.new ENV['flicker_api_key'], ENV['flicker_api_secret']
+
+   
+
+  end
+
 end
